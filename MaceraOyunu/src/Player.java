dc6afa9aa79bd3c698f35003d2d4a7858de64f27
Esra <59251830+Esraerdogan@users.noreply.github.com>
2023-02-19 11:48:39 +0300

    Add files via upload

---
+++ MaceraOyunu/src/Player.java
@@ -0,0 +1,125 @@
+import java.util.Scanner;
+
+public class Player  {
+    private int damage;
+    private int health;
+    private int money;
+    private String charName;
+    private String name;
+    Scanner scanner = new Scanner(System.in);
+    private Inventory inventory;
+
+    public Player(String name) {
+        this.name = name;
+        this.inventory = new Inventory();
+    }
+    public void selectChar() {
+
+        Character[] characters = { new Samurai(), new Archer(), new Knight() };
+        System.out.println("----------------------------------------------");
+
+        for (Character character : characters) {
+            System.out.println("ID:" + character.getId() +
+                    "\t\tKarakter Adı:" + character.getCharName() +
+                    "\t\tHasar:" + character.getDamage() +
+                    "\t\tCan:" + character.getHealth() +
+                    "\t\tPara:" + character.getMoney());
+
+        }
+        System.out.println("------------------------------------------");
+        System.out.print("Lütfen bir karakter seçiniz:");
+        int selectChar = scanner.nextInt();
+
+        switch (selectChar) {
+            case 1:
+                initPlayer(new Samurai());
+                break;
+            case 2:
+                initPlayer(new Archer());
+                break;
+            case 3:
+                initPlayer(new Knight());
+                break;
+            default:
+                initPlayer(new Samurai());
+        }
+            /*
+            System.out.println(
+                    "Seçtiğiniz karakter:" + this.getCharName() +
+                    "\t\tHasar:" + this.getDamage() +
+                    "\t\tCan:" + this.getHealth() +
+                    "\t\tPara:" + this.getMoney());
+            */
+    }
+
+
+
+    public void initPlayer(Character character) {
+        this.setDamage(character.getDamage());
+        this.setHealth(character.getHealth());
+        this.setMoney(character.getMoney());
+        this.setCharName(character.getCharName());
+    }
+
+    public void printInfo(){
+        System.out.println(
+                "Silahınız : " + this.getInventory().getWeapon().getName() +
+                        " Zırhınız : " + this.getInventory().getArmor().getName() +
+                        " Bloklama : " + this.getInventory().getArmor().getAvoid() +
+                        " Hasarınız : " + this.getTotalDamage() +
+                        " Sağlık : " + this.getHealth() +
+                        " Para : " + this.getMoney()
+        );
+    }
+
+    public int getTotalDamage(){
+        return this.getDamage() + this.getInventory().getWeapon().getDamage();
+    }
+    public int getDamage() {
+        return damage + this.getInventory().getWeapon().getDamage();
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+    public String getCharName() {
+        return charName;
+    }
+
+    public void setCharName(String charName) {
+        this.charName = charName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Inventory getInventory() {
+        return inventory;
+    }
+    public void setInventory(Inventory inventory) {
+        this.inventory = inventory;
+    }
+
+}
---
+++ MaceraOyunu/src/ToolStore.java
@@ -0,0 +1,97 @@
+public class ToolStore extends NormalLoc {
+    public ToolStore(Player p) {
+        super(p, "Mağaza");
+    }
+
+    @Override
+    public boolean onLocation() {
+        System.out.println("------------Mağazaya hoşgeldiniz-----------");
+        System.out.println("1 - Silahlar");
+        System.out.println("2 - Zırhlar");
+        System.out.println("3 - Çıkış");
+        System.out.print("Seçim yapınız: ");
+        int selectCase = Location.scanner.nextInt();
+        while (selectCase < 1 || selectCase > 3) {
+            System.out.println("Geçersiz değer tekrar deneyiniz.");
+            selectCase = Location.scanner.nextInt();
+        }
+        switch (selectCase) {
+            case 1:
+                printWeapon();
+                buyWeapon();
+                break;
+            case 2:
+                printArmor();
+                buyArmor();
+                break;
+            case 3:
+                System.out.println("Bir daha bekleriz");
+                break;
+        }
+        return true;
+    }
+
+    public void printWeapon() {
+        System.out.println("---------Silahlar-----------");
+        System.out.println();
+        for (Weapon w : Weapon.weapons()) {
+            System.out.println(w.getName() + "<Para : " + w.getPrice() + " Hasar : " + w.getDamage() + ">");
+        }
+
+    }
+
+    public void buyWeapon() {
+        System.out.print("Bir silah seçiniz : ");
+        int selectWeaponID = scanner.nextInt();
+        while (selectWeaponID < 1 || selectWeaponID > Weapon.weapons().length) {
+            System.out.print("Geçersiz değer. Tekrar giriniz : ");
+            selectWeaponID = scanner.nextInt();
+        }
+        Weapon selectedWeapon = Weapon.getWeaponObjById(selectWeaponID);
+        if (selectedWeapon != null) {
+            if (selectedWeapon.getPrice() > this.getPlayer().getMoney()) {
+                System.out.println("Yeterli Paranız Bulunmamaktadır");
+            } else {
+                System.out.println(selectedWeapon.getName() + " silahını satın aldınız.");
+                int balance = this.getPlayer().getMoney() - selectedWeapon.getPrice();
+                this.getPlayer().setMoney(balance);
+                System.out.println("Kalan paranız : " + this.getPlayer().getMoney());
+                System.out.println("Önceki silahınız : " + this.getPlayer().getInventory().getWeapon().getName());
+                this.getPlayer().getInventory().setWeapon(selectedWeapon);
+                System.out.println("Şimdiki silahınız : " + this.getPlayer().getInventory().getWeapon().getName());
+            }
+        }
+    }
+
+    public void printArmor() {
+        System.out.println("---------Zırhlar---------");
+        for (Armor a : Armor.armors()) {
+            System.out.println(
+                    a.getId() + " - " + a.getName() + "<Para : " + a.getPrice() + " Zırh : " + a.getBlock() + ">");
+        }
+    }
+
+    public void buyArmor() {
+        System.out.print("Bir zırh seçiniz : ");
+        int selectArmorID = scanner.nextInt();
+        while (selectArmorID < 1 || selectArmorID > Armor.armors().length) {
+            System.out.print("Geçersiz değer. Tekrar giriniz : ");
+            selectArmorID = scanner.nextInt();
+            Armor selectedArmor = Armor.getArmorObjById(selectArmorID);
+            if (selectedArmor != null) {
+                if (selectedArmor.getPrice() > this.getPlayer().getMoney()) {
+                    System.out.println("Yeterli Paranız Bulunmamaktadır");
+                } else {
+                    System.out.println(selectedArmor.getName() + " zırhının satın aldınız.");
+                    int balance = this.getPlayer().getMoney() - selectedArmor.getPrice();
+                    this.getPlayer().setMoney(balance);
+                    System.out.println("Kalan paranız : " + this.getPlayer().getMoney());
+                    System.out.println("Önceki zırhınız : " + this.getPlayer().getInventory().getArmor().getName());
+                    this.getPlayer().getInventory().setArmor(selectedArmor);
+                    System.out.println("Şimdiki zırhınız : " + this.getPlayer().getInventory().getArmor().getName());
+                }
+            }
+        }
+
+    }
+}
